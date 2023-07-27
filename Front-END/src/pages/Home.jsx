a3e00070f94dbcd3e960d312cc0151ccc33af19c
Graziano Corineo <grazianocorineo01@gmail.com>
2023-07-27 17:50:45 +0200

    Update Organzation fold components

---
+++ Front-END/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Home = () => {
+
+  return (
+    <>
+     <h1>Hello Word</h1>
+     <div>
+      <Button variant="primary">Click Me</Button>
+    </div>
+    <div>
+      <Button variant="primary">Cliccami</Button>
+    </div>
+    </>
+  )
+}
+
+export default Home;
