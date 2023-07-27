44a8e05026546c2f4f33b0ff83fabbbeb3ddf399
Graziano Corineo <grazianocorineo01@gmail.com>
2023-07-27 17:21:05 +0200

    Update bootstrap react-bootstrap

--- Front-END/src/App.jsx
+++ Front-END/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react"
+import { Button } from 'react-bootstrap';
 
 function App() {
 
   return (
     <>
      <h1>Hello Word</h1>
+     <div>
+      <Button variant="primary">Click Me</Button>
+    </div>
+    <div>
+      <Button variant="primary">Cliccami</Button>
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
