f2c518ece0e4e7f5074ff6679fc41efc97c814b7
PaoloBaiada <paolosettemaggio@gmail.com>
2023-07-28 13:08:34 +0200

    merge done

--- Front-END/src/App.jsx
+++ Front-END/src/App.jsx
@@ -6,11 +6,9 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-     <Home/>
-=======
+
     <Home/>
->>>>>>> main
+
     </>
   )
 }
--- Front-END/src/pages/Home.jsx
+++ Front-END/src/pages/Home.jsx
@@ -5,19 +5,11 @@ const Home = () => {
   return (
     <>
      <h1>Hello Word</h1>
-<<<<<<< HEAD
-     <div className="btn btn-primary mb-3">
-     click me
-    </div>
-    <div>
-      <ConsigliatoPerTe/>
-=======
      <div className="btn btn-primary">
       click me
     </div>
     <div>
       
->>>>>>> main
     </div>
     </>
   )
