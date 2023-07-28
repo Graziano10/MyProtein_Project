4bdee57208ec2aa30ec3f0f1347c6d485272dc3b
PaoloBaiada <paolosettemaggio@gmail.com>
2023-07-28 12:31:23 +0200

    custom variables

--- Front-END/src/pages/Home.jsx
+++ Front-END/src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React from "react"
-import {Button} from "react-bootstrap"
 import ConsigliatoPerTe from "../components/bundle/ConsigliatoPerTe";
 const Home = () => {
 
   return (
     <>
      <h1>Hello Word</h1>
-     <div className="btn btn-secondary mb-3">
+     <div className="btn btn-primary mb-3">
      click me
     </div>
     <div>
-      {<Button variant="primary">Cliccami</Button>}
       <ConsigliatoPerTe/>
     </div>
     </>
